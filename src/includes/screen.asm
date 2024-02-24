415fc4e096af52c6dc95ec5e4a49d08f1bd62cd9
Rodrigo Laneth <rlaneth@miralium.re>
2024-02-24 17:29:20 -0300

    Update menu text

--- src/includes/screen.asm
+++ src/includes/screen.asm
@@ -6,28 +6,22 @@ SCREEN_WIDTH      equ 76
 SCREEN_ATTR_TITLE equ DEFAULT_ATTR_TITLE
 
 SCREEN_MAIN       db 'Welcome!', 0x00
-
-                  db 'Magnolia is an open source platform that brings '
+                  db 'Magnolia is an open source platform that brings ',
                   db 'applications directly to', 0x0A,
                   db `your PC's BIOS! Visit our repository on GitHub:`,
                   db 0x0A, 'https://github.com/miraliumre/magnolia',
                   db 0x00
 
 SCREEN_GAMES      db 'Games', 0x00
+                  db 'This menu provides access to a selection of ',
+                  db 'simple yet engaging games which ', 0x0A,
+                  db 'can run directly from Magnolia.', 0x00
 
-                  db 'Immerse yourself in pure gaming entertainement '
-                  db 'directly within your BIOS!', 0x0A
-                  db 'Experience the joy of lauching games without the '
-                  db 'need for any operating', 0x0A
-                  db 'system in between.', 0x00
-
-SCREEN_TOOLS      db 'Tools', 0x00
-
-                  db 'Discover an ever-expanding collection of '
-                  db 'utilities designed to help you', 0x0A
-                  db `delve deeper into your hardware's capabilities. `
-                  db 'As Magnolia evolves, we ', 0x0A
-                  db 'will introduce more tools to this section.', 0x00
+SCREEN_TOOLS      db 'Tools', 0x00,
+                  db 'This menu offers utility applications designed ',
+                  db 'to assist with system-level ', 0x0A,
+                  db 'tasks related to programming, reverse ',
+                  db 'engineering, and hardware diagnostics.', 0x00
 
 draw_screen:
     pusha
